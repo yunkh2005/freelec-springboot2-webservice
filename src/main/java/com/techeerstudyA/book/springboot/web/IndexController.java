3b74c4594ba49240c6b1a9cd90e3e957e38f27b8
yoon <yunkh2005@naver.com>
2022-12-23 22:34:48 +0900

    5장 실습

--- src/main/java/com/techeerstudyA/book/springboot/web/IndexController.java
+++ src/main/java/com/techeerstudyA/book/springboot/web/IndexController.java
@@ -1,21 +1,29 @@
 package com.techeerstudyA.book.springboot.web;
 
+import com.techeerstudyA.book.springboot.config.auth.LoginUser;
 import com.techeerstudyA.book.springboot.service.PostsService;
 import com.techeerstudyA.book.springboot.web.dto.PostsResponseDto;
+import com.techeerstudyA.book.springboot.web.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
